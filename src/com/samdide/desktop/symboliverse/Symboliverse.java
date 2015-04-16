1ea8bf652bd5ec65b9d67f105dbaa6fcdd49a99c
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-04-17 00:18:05 +0200

    Tested to draw a crude class diagram.

--- src/com/samdide/desktop/symboliverse/Symboliverse.java
+++ src/com/samdide/desktop/symboliverse/Symboliverse.java
@@ -6,20 +6,41 @@
 package com.samdide.desktop.symboliverse;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Symboliverse extends Application{
 
 	@Override
 	public void start(Stage primaryStage) {
-		Button btOK = new Button("An UML diagram tool set");
-		Scene scene = new Scene(btOK, 200, 250);
 		primaryStage.setTitle("Symboliverse");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		
+		Group root = new Group();
+	    Canvas canvas = new Canvas(300, 250);
+	    GraphicsContext gc = canvas.getGraphicsContext2D();
+	    drawShapes(gc);
+	    root.getChildren().add(canvas);
+	    primaryStage.setScene(new Scene(root));
+	    primaryStage.show();
 	}
+	
+	 private void drawShapes(GraphicsContext gc) {
+	        gc.setStroke(Color.BLUE);
+	        gc.setLineWidth(2);
+	        gc.strokeRect(100, 60, 150, 130);
+	        gc.strokeLine(100, 90, 250, 90);
+	        gc.strokeLine(100, 120, 250, 120);
+	        gc.setLineWidth(1);
+	        gc.setFont(new Font("Consolas", 14));
+	        gc.fillText("BankAccount", 120, 80);
+	 }
+	
 	public static void main(String[] args) {
 		Application.launch(args);
 
